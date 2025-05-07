1e7c7a0bae73a395eec52d14987ddf5aa97454e6
renee <156127959+reneexie01@users.noreply.github.com>
2025-05-08 06:39:57 +1000

    Adding labels and input ids and types

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -13,8 +13,13 @@ function Inputs() {
 function GeneralInformationInputs() {
     return (
         <>
-        <p>Name: </p>
-        <input />
+        <h1>General Information: </h1>
+        <label for="full-name">Full name: </label>
+            <input id="full-name" />
+        <label for="email">Email: </label>
+            <input id="email"/>
+        <label for="phone-number">Phone number: </label>
+            <input id="phone-number"/>
         </>
     )
 }
@@ -22,8 +27,15 @@ function GeneralInformationInputs() {
 function EducationInputs() {
     return (
         <>
-        <p>Education: </p>
-        <input />
+        <h1>Education: </h1>
+        <label for="school">School: </label>
+            <input id="school"/>
+        <label for="course">Course: </label>
+            <input id="course"/>
+        <label for="start-date">Start date: </label>
+            <input id="education-start-date" type="date"/>
+        <label for="end-date">End date: </label>
+            <input id="education-end-date" type="date"/>
         </>
     )
 }
@@ -31,8 +43,17 @@ function EducationInputs() {
 function ExperienceInputs() {
     return (
         <>
-        <p>Experience: </p>
-        <input />
+        <h1>Experience: </h1>
+        <label for="company">Company name: </label>
+            <input id="company"/>
+        <label for="position">Position title: </label>
+            <input id="position"/>
+        <label for="responsibilities">Main responsibilities: </label>
+            <input id="responsibilities"/>
+        <label for="experience-start-date">Start date: </label>
+            <input id="experience-start-date" type="date"/>
+        <label for="experience-end-date">End date: </label>
+            <input id="experience-end-date" type="date"/>
         </>
     )
 }
